1801a32d719c598117f1e05d82fede09f9c9a02e
Glauber-Nunes <nglauber53@gmail.com>
2023-10-08 13:59:46 -0300

    atualizaçao

---
+++ src/main/java/com/gnsoftware/Ordem/Servico/dto/PagamentoDto.java
@@ -0,0 +1,7 @@
+package com.gnsoftware.Ordem.Servico.dto;
+
+public class PagamentoDto {
+
+    private Long id;
+
+}
---
+++ src/main/java/com/gnsoftware/Ordem/Servico/dto/VendaDto.java
@@ -0,0 +1,6 @@
+package com.gnsoftware.Ordem.Servico.dto;
+
+public class VendaDto {
+
+    private Long id;
+}
