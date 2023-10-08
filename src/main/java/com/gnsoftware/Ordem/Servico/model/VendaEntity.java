6d599671e5e0b7ea1b310ed4c0091f4348631d56
Glauber-Nunes <nglauber53@gmail.com>
2023-10-08 14:21:47 -0300

    att

--- src/main/java/com/gnsoftware/Ordem/Servico/model/VendaEntity.java
+++ src/main/java/com/gnsoftware/Ordem/Servico/model/VendaEntity.java
@@ -1,17 +1,36 @@
 package com.gnsoftware.Ordem.Servico.model;
 
 import com.gnsoftware.Ordem.Servico.model.compositekey.ProdutoOrdemEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Table(name = "venda")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class VendaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private ClienteEntity cliente;
+    @ManyToOne
+    @JoinColumn(name = "atendente_id")
     private AtendenteEntity atendente;
+
     private List<ProdutoOrdemEntity> produtos = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "pagamento_id")
     private PagamentoEntity pagamento;
     private String observacoes;
     private Double totalVenda;
